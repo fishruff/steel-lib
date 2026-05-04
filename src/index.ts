b62b15c53e1d79e2ddab12603a5792a01c057338
fishruff <arsershoff@gmail.com>
2026-05-04 19:10:18 +0500

    range

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 type Steel ={
     name: string,
     standard?:string,
-    C: number,
+    C: {
+        min:number,        
+        max:number,        
+    },
     Mn: number,
     yieldStrength:number
 }
 
 const steelArr: Steel[] = [
-    { name: "09Г2С", C: 0.09, Mn: 1.6, yieldStrength: 345 },
-    { name: "Ст3", C: 0.14, Mn: 0.5, yieldStrength: 235 },
-    { name: "20", C: 0.2, Mn: 0.5, yieldStrength: 245 },
-    { name: "30", C: 0.3, Mn: 0.6, yieldStrength: 355 },
-    { name: "40Х", C: 0.4, Mn: 0.8, yieldStrength: 600 },
-    { name: "30ХГСА", C: 0.3, Mn: 0.8, yieldStrength: 700 },
-    { name: "15ГС", C: 0.15, Mn: 1.3, yieldStrength: 390 },
-    { name: "10Г2", C: 0.1, Mn: 1.5, yieldStrength: 345 },
-    { name: "35", C: 0.35, Mn: 0.7, yieldStrength: 500 },
-    { name: "45", C: 0.45, Mn: 0.7, yieldStrength: 600 }
+    { name: "09Г2С", C: {"min": 0.17, "max": 0.24}, Mn: 1.6, yieldStrength: 345 },
+    { name: "Ст3", C: {"min": 0.17, "max": 0.24}, Mn: 0.5, yieldStrength: 235 },
+    { name: "20", C: {"min": 0.17, "max": 0.24}, Mn: 0.5, yieldStrength: 245 },
+    { name: "30", C: {"min": 0.17, "max": 0.24}, Mn: 0.6, yieldStrength: 355 },
+    { name: "40Х", C: {"min": 0.17, "max": 0.24}, Mn: 0.8, yieldStrength: 600 },
+    { name: "30ХГСА", C: {"min": 0.17, "max": 0.24}, Mn: 0.8, yieldStrength: 700 },
+    { name: "15ГС", C: {"min": 0.17, "max": 0.24}, Mn: 1.3, yieldStrength: 390 },
+    { name: "10Г2", C: {"min": 0.17, "max": 0.24}, Mn: 1.5, yieldStrength: 345 },
+    { name: "35", C: {"min": 0.17, "max": 0.24}, Mn: 0.7, yieldStrength: 500 },
+    { name: "45", C: {"min": 0.19, "max": 0.34}, Mn: 0.7, yieldStrength: 600 }
   ]
 
   const getSteel = (name:string)=>{
@@ -34,7 +37,7 @@ const compareSteel=(aName:string, bName:string)=>{
 }
 
 const calculateSimilarity= (a:Steel, b:Steel)=>{
-    const Cdiff = Math.abs(a.C - b.C);
+    const Cdiff = Math.abs(a.C.max - b.C.max);
     const Mndiff = Math.abs(a.Mn - b.Mn);
     const Yielddiff =  Math.abs(a.yieldStrength - b.yieldStrength);
 
@@ -69,5 +72,31 @@ const findSimilar = (s: string)=>{
     .sort((a, b) => b.similarity - a.similarity);
     return result
 }
-console.log(findSimilar("09Г2С"))
-// console.log(compareSteel("09Г2С", "Ст3"))
+
+type Range1 = {
+    min: number,
+    max: number
+}
+
+
+const compareRange=(a: Range1, b: Range1): number=>{
+    const overlap = 
+    Math.min(a.max, b.max) - 
+    Math.max(a.min, b.min);
+
+    if (overlap<0) return 0
+    
+
+    const total = 
+    Math.max(a.max, b.max)-
+    Math.min(a.min, b.min); 
+
+    if (total===0) return 1
+    
+
+    const similarity = overlap/total;
+
+    return similarity
+}
+console.log(compareRange({min:0.12, max:0.30},{min:0.17, max:0.34}))
+// console.log(findSimilar("09Г2С"))
